54af697375658feedcf405111604597851deca96
Adrian Abendroth <abendroth.adrian@yahoo.de>
2017-12-23 17:19:15 +0100

    Setter und Getter fuer die Klasse Benutzer gemacht

--- src/model/Benutzer.java
+++ src/model/Benutzer.java
@@ -1,5 +1,108 @@
 package model;
 
 public class Benutzer {
+	// attributs 
+	protected String nachname;
+	protected String vorname;
+	protected String email;
+	protected String passwort;
+	protected String geschlecht;
+	protected int speicherplatz;
+	protected boolean werbung;
+	
+	// methods
+	
+	 public static boolean Authenticate(String username, String password) {
+	        // hardcoded username and password
+	        if (username.equals("Adrian") && password.equals("1337")) {
+	            return true;
+	        }
+	        return false;
+	    }
+	 
+	 /******* Getter ******* /
+	 
+	 /**
+	  * @return nachname
+	  */
+	 public String getNachname() {
+		 return nachname;
+	 }
+	 
+	 /**
+	  * @return vorname
+	  */
+	 public String getVorname() {
+		 return vorname;
+	 }
+	 
+	 /**
+	  * @return email
+	  */
+	 public String getemail() {
+		 return email;
+	 }
+	 
+	 /**
+	  * @return passwort
+	  */
+	 public String getPasswort() {
+		 return passwort;
+	 }
+	 
+	 /**
+	  * @return geschlecht
+	  */
+	 public String getGeschlecht() {
+		 return geschlecht;
+	 }
+	 
+	 /**
+	  * @return speicherplatz
+	  */
+	 public int getSpeicherplatz() {
+		 return speicherplatz;
+	 }
+	 
+	 /**
+	  * @return werbung
+	  */
+	 public boolean getWerbung() {
+		 return werbung;
+	 }
+	 
+	 /******* Getter *******/
+	 
+	 public void setNachname(String name) {
+		 this.nachname = name;
+	 }
+	 
+	 public void setVorname(String vorname) {
+		 this.vorname = vorname;
+	 }
+	 
+	 public void setEmail(String email) {
+		 this.email = email;
+	 }
+	 
+	 public void setPasswort(String passwort) {
+		 this.passwort = passwort;
+	 }
+	 
+	 public void setGeschlecht(String geschlecht) {
+		 this.geschlecht = geschlecht;
+	 }
+	 
+	 public void setSpeicherplatz(int speicherplatz) {
+		 this.speicherplatz = speicherplatz;
+	 }
+	 
+	 public void setWerbung(boolean werbung) {
+		 this.werbung = werbung;
+	 }
+	 
 
+
+	
+	
 }
---
+++ src/model/LoginMain.java
@@ -0,0 +1,17 @@
+package model;
+
+public class LoginMain {
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+	    // TODO code application logic here
+	   Benutzer benutzerObjekt = new Benutzer();
+	   benutzerObjekt.setNachname("test");
+	   benutzerObjekt.setPasswort("ola");
+	   System.out.println(benutzerObjekt.getPasswort());
+	   
+	}
+
+}
